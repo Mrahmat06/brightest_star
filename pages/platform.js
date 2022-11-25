05cd6aedaf241c13dc185db73d5286c510bce873
Rahmat Mukhalin <mukhalin2001@gmail.com>
2022-11-25 20:56:19 +0700

    first commit

---
+++ pages/platform.js
@@ -0,0 +1,143 @@
+import Header from "../components/Header";
+import Meta from "../components/Meta";
+import Footer from "../components/Footer";
+import Image from "next/image";
+import steam from "../public/assets/steam.png";
+import epic from "../public/assets/epicgames.png";
+import riot from "../public/assets/riot.png";
+import origin from "../public/assets/origin.png";
+import rockstar from "../public/assets/rockstar.png";
+import battlenet from "../public/assets/batlenett.png";
+import Navbar from "../components/Navbar";
+import image from "../public/assets/reaksibintang.jpg";
+import blackhole from "../public/assets/blackhole.jpg";
+import lightspeed from "../public/assets/lightspeed.jpg";
+import gemini from "../public/assets/gemini.jpg";
+export default function Tools() {
+  return (
+    <div>
+      <Meta title="9 BRIGHEST STAR IN ASTRONOMY" />
+      <Header />
+      <section class="text-gray-600 bg-black body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="text-2xl font-medium title-font mb-4 text-slate-200">Star Funfact</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Beberapa fakta menarik tentang bintang.</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center">
+          <Image alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src={image} width={200} height={200}/>
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-slate-200">Terbentuk dari hidrogen dan helium</h2>
+            <h3 class="text-gray-500 mb-3">    </h3>
+            <p class="mb-4">Adanya kandungan hidrogen dan helium pada bintang membuat benda langit yang satu ini dapat memancarkan cahayanya sendiri.</p>
+            <span class="inline-flex">
+              <a class="text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a class="ml-2 text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a class="ml-2 text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                </svg>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center">
+          <Image alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src={blackhole} width={100} height={100}/>
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-slate-200">Bintang akan lahir dan akan lenyap</h2>
+            <h3 class="text-gray-500 mb-3"></h3>
+            <p class="mb-4">Bintang akan lenyap jika kekurangan energi nuklir. Mereka akan meledak dan menghasilkan partikel lain, salah satunya adalah black hole..</p>
+            <span class="inline-flex">
+              <a class="text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a class="ml-2 text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a class="ml-2 text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                </svg>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center">
+          <Image alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src={lightspeed} width={200} height={200}/>
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-slate-200">Cahaya bintang berasal dari masa lalu</h2>
+            <h3 class="text-gray-500 mb-3"></h3>
+            <p class="mb-4">Karna bintang berada sangat jauh dari bumi, maka cahaya yang kita lihat sekarang berasan dari masa lalu akibat jarak yang sangat jauh tersebut.</p>
+            <span class="inline-flex">
+              <a class="text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a class="ml-2 text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a class="ml-2 text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                </svg>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 lg:w-1/4 md:w-1/2">
+        <div class="h-full flex flex-col items-center text-center">
+          <Image alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src={gemini} width={200} height={200}/>
+          <div class="w-full">
+            <h2 class="title-font font-medium text-lg text-slate-200">Rasi bintang diamati sejak zaman dahulu</h2>
+            <h3 class="text-gray-500 mb-3"></h3>
+            <p class="mb-4">penduduk Yunani kuno sudah memahami rasi bintang yang memanfaatkannya untuk berbagai keperluan, seperti contohnya navigasi.</p>
+            <span class="inline-flex">
+              <a class="text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a class="ml-2 text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a class="ml-2 text-gray-500">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                  <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                </svg>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
